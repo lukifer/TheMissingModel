7a3f488bd2a9983a90d4f44ce0dae4c1c1ae7940
Luke Dennis <ldennis@blueriotlabs.com>
2013-01-23 19:24:35 -0700

    Initial commit and documentation

--- README.md
+++ README.md
@@ -2,3 +2,100 @@ TheMissingModel
 ===============
 
 A small, chainable model class for CodeIgniter, loosely inspired by jQuery
+
+Requires CodeIgniter 2.1+
+
+
+Getting Started
+---------------
+
+- Add MY_Model.php to your "application/core" folder (create it if it does not exist).
+
+- Create your models objects as subclasses of MY_Model, like so: ```class User extends MY_Model```
+
+- See the [CI User Guide](http://ellislab.com/codeigniter/user-guide/general/creating_libraries.html) for more details.
+
+
+Usage
+-----
+
+This class turns every model object into a container for data, similar to the way that jQuery is a container for DOM objects. Data is stored in an internal array ($Obj->data). This data can be worked with as though it were multiple items, or a single item; if acting on a single item, such as calling $Obj->value(), the object acts only on the first item.
+
+Each model is intended to be tied to a specific table. In your model subclass, set this value with the name of that table: ```public $table = 'my_table';```. Though each model is meant to represent one table, you don't have to make a model for every table, and you can still do JOINs as part of your queries (see below). Or, if you don't wish to tie your model to a table (for instance, when using an Entity-Attribute-Value schema), you can leave the $table attribute blank, in which case the ->query(), ->save(), and ->insert() methods will not function.
+
+Most actions return a copy of the object itself, allowing actions to be chained on the same object, similarly to jQuery or CodeIgniter's ActiveRecord. It is encouraged to follow this convention on your own Model objects as well (```return $this;```). Success and failure should be determined based on the internal $Obj->errors array, and/or the presence of data.
+
+One last tidbit: This class assumes you use 'id' as a primary key. If you use a different primary key, be sure to change this value in your subclassed model, or pass in the desired value to the relevant function(s).
+
+
+Details
+-------
+
+The class itself is short and sweet, and you can treat the code as documentation. But for convenience, here are the included attributes and methods:
+
+#### Attributes ####
+
+- **$table**: Full name of primary database table.
+- **$data**: Internal data storage.
+- **$errors**: Internal array of errors. Error strings are recommended, but can be numbers, arrays, or objects if desired.
+- **$validation**: An optional array for CodeIgniter's (Form Validation)[http://ellislab.com/codeigniter/user-guide/libraries/form_validation.html] functionality. Allows you to keep validation rules in the model instead of the controller.
+
+
+#### Methods ####
+
+- **->sql("SELECT * FROM my_table")**: Populates internal data with results of SQL query.
+- **->query(array('field_name' => 'some_value'), 'field1, field2')**: Simple WHERE queries, and optional SELECT.
+
+- **->getAll()**: Return array of all internal data.
+- **->getOne()**: Return a single item and increment counter. Useful for looping.
+- **->get(5)**: Get a specific item if present. Internal data is numerically indexed, starting at zero.
+- **->getIndexed()**: Retrieve all data, indexed by primary key or the supplied field
+- **->getKeyValue('some_field')**: Create key-value pairs from internal data. Useful for populating <option> elements. Defaults to array('id' => 'name').
+- **->rewind()**: Reset the internal counter for getOne().
+
+- **->indexArray()**: Utility function. Does the same thing as getIndexed(), but on any array.
+- **->keyValue()**: Utility function. Does the same thing as getKeyValue(), but on any array.
+
+- **->value('some_field')**: Get the desired field for the current item in the array (usually the first).
+- **->setValue('some_field', 'a value')**: Set the desired field for the current item in the array (usually the first).
+
+- **->save()**: Save all internal data to database. Intelligently UPDATEs or INSERTs based upon the presence of a primary key in the data.
+- **->insert(array('some_field' => 'a value'))**: INSERT supplied array into database, and load into internal data, including the resultant primary key.
+
+- **->validate()**: Wrapper for CodeIgniter's Form Validation run() function. Uses rules set in the $validation attribute.
+- **->error("Something happened")**: Adds an error to the internal error stack.
+- **->success()**: Returns true if there are no internal errors.
+- **->clearErrors()**: Deletes all internal errors.
+- **->clear()**: Deletes all internal data.
+
+
+#### Simple JOINs ####
+
+If you pass an additional array to the ->query() function, it will attempt to LEFT JOIN using the supplied keys and values, like so:
+
+```$Obj->query(array('food' => 'tacos'), array('other_table', 'other_table.linking_id = main_table.id'), '*')```
+
+
+#### Complex Data Relationships ####
+
+The simple way to use this class is for small wrappers for specific tables. However, you can also use it for complex multi-table relationships, by creating your own functions for specific queries, and/or over-riding the standard functions such as ->query() and ->save().
+
+
+#### Saving Multi-Table Data ####
+
+If you are retrieving fields other than your primary table, whether via JOINs, custom queries, or SQL functions such as SUM(), you will need to specify which fields to update when using the ->save() function, like so:
+
+```$Obj->save(array('some_field', 'some_other_field'));```
+
+By default, ->save() will attempt to save all fields it finds, which works great with single-table data, but other-wise will throw an error.
+
+
+#### One Last Thing ####
+
+You can choose on a per-model basis whether or not to use this model class, if so desired. To use CodeIgniter's vanilla model, just use ```class User extends CI_Model``` just like normal.
+
+
+Example
+-------
+
+Coming Soon!
---
+++ application/core/MY_Model.php
@@ -0,0 +1,294 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+    
+# THE MISSING MODEL: A small, chainable model class for CodeIgniter, loosely inspired by jQuery
+# 
+# MIT Licensed: http://opensource.org/licenses/MIT
+# https://github.com/lukifer/TheMissingModel
+
+
+if (!class_exists('CI_Model')) require_once (BASEPATH . 'core/Model.php');
+
+class MY_Model extends CI_Model {
+	
+	public $table = ''; # required in subclass
+	public $primaryKey = 'id';
+	
+	public $data = array();
+	public $validation = array();
+	public $errors = array();
+	
+	# Convenience functions
+	public function count()		{ return count($this->data);		}
+	public function hasData()	{ return count($this->data) > 0;	}
+	
+	
+	# Populate internal data with manual SQL 
+	public function sql($queryString)
+	{
+		$sql = $this->db->query($queryString);
+
+		if(!$sql->num_rows())
+			$this->data = array();
+		else
+			$this->data = $sql->result_array();
+		
+		return $this;
+	}
+	
+	
+	# Simple WHERE + LEFT JOIN. More complicated queries should use ->sql(), or custom functions
+	public function query($where, $join = array(), $select = '*')
+	{
+		if(is_string($join))
+		{
+			$select = $join;
+			$join = array();
+		}
+	
+		$this->db->select($select);
+	
+		if(is_numeric($where))
+			$this->db->where($this->table.'.'.$this->primaryKey, $where)->limit(1);
+
+		else if(is_array($where) && !empty($where))
+			foreach($where as $k => &$v)
+			{
+				if(!strchr($k, '.')) $k = $this->table.'.'.$k;
+				$this->db->where($k, $v);
+			}
+		
+		# Quick and dirty joining. Not recommended for complex queries.
+		if(!empty($join))
+		{	if(!is_array($join)) $join = array($join);
+			foreach($join as $k => &$j)
+				$this->db->join($k, $j, 'left');
+		}		
+		
+		$sql = $this->db->get($this->table);
+		$this->data = $sql->result_array();
+
+		return $this;
+	}
+	
+	
+	# Retrieve all internal data
+	public function getAll()
+	{
+		return $this->get();
+	}
+	
+	
+	# Get a specific item
+	public function get($k)
+	{
+		if(empty($this->data)) return false;
+	
+		else if(isset($this->data[$k]))
+			return $this->data[$k];
+		
+		else return false;
+	}
+	
+	
+	# Get the next item, and increment counter. Useful for looping.
+	public function getOne($first = false)
+	{
+		if(empty($this->data)) return false;
+	
+		if($first) reset($this->data);
+		return current($this->data);
+	}
+	
+	
+	public function rewind()
+	{
+		if(empty($this->data)) return false;
+		
+		reset($this->data);
+		
+		return $this;
+	}
+	
+	
+	# Get all results as an associative array, indexed by primary key or the specified field name
+	public function getIndexed($keyField = false)
+	{
+		if($keyField === false) $keyField = $this->primaryKey;
+		return $this->indexArray($this->data, $keyField);
+	}
+	
+	
+	# Utility function to create an associative array. Does not affect internal data.
+	public function indexArray($array, $field = false)
+	{
+		$indexed = array();
+		if($field === false) $field = $this->primaryKey;
+	
+		if(!empty($array)) foreach($array as &$item)
+			$indexed[$item[$field]] = $item;
+		
+		return $indexed;
+	}
+	
+
+	# Create a key-value store, such as for building <option> elements. Does not affect internal data. 
+	public function getKeyValue($nameField = 'name', $keyField = false)
+	{	
+		if($keyField === false) $keyField = $this->primaryKey;
+		return $this->createKeyValue($this->data, $nameField, $keyField);
+	}
+
+
+	# Utility function to create a key-value store. 
+	public function keyValue($array, $nameField = 'name', $keyField = false)
+	{	
+		if($keyField === false) $keyField = $this->primaryKey;
+		
+		$newArray = array();
+		if(!empty($array)) foreach($array as $k => &$a)
+		{
+			$key = $a[$keyField];
+			if(empty($key)) $newArray[$k] = $a[$nameField];
+			else $newArray[$key] = $a[$nameField];
+		}
+		return $newArray;
+	}
+	
+	
+	# Get a value from the first item in internal data
+	public function value($field)
+	{
+		if($this->count() < 1) return null;
+		
+		$current = current($this->data);
+		if(empty($current)) return false;
+		
+		return isset($current[$field]) ? $current[$field] : false;
+	}
+
+
+	# Set a value on the first item in internal data
+	public function setValue($field, $val)
+	{
+		if($this->count() < 1) return $this;
+
+		$key = key($this->data);
+		if(@empty($this->data[$key])) return $this;
+		
+		$this->data[$key][$field] = $val;
+		
+		return $this;
+	}
+
+
+	# Save internal data to database, doing an INSERT or UPDATE based on presence of primary key
+	# If only some fields should be saved, pass the field keys in the first argument
+	public function save($fields = false, $primaryKey = false)
+	{
+		if($primaryKey === false) $primaryKey = $this->primaryKey;
+
+		# If passed a string (single field), convert it to an array
+		if($fields !== false && !is_array($fields))
+			$fields = array($fields);
+
+		# Loop through internal data, saving each
+		if(!empty($this->data)) foreach($this->data as $datumIndex => $datum)
+		{
+			# No fields provided: save all data
+			if(!$fields)
+			{
+				$fields = array();
+				foreach($datum as $k => $v)
+					if($k != $primaryKey) $fields[] = $k;
+				if(empty($fields)) continue;
+			}
+	
+			# Otherwise, save only the requested field(s)
+			foreach($fields as &$f)
+				if(isset($datum[$f])) $this->db->set($f, $datum[$f]);
+
+			# No primary key: INSERT, and if successful, save the resulting primary key
+			if(!isset($datum[$primaryKey]))
+			{	
+				$this->db->insert($this->table, $datum);
+				
+				if($this->db->affected_rows() == 1)
+					$this->data[$datumIndex][$primaryKey] = $this->db->insert_id();
+			}
+			
+			# Primary key present: UPDATE instead
+			else
+			{
+				$this->db->where($primaryKey, $datum[$primaryKey]);
+				$this->db->update($this->table);
+		
+				# Unfortunately, mysql_affected_rows() can't be trusted with UPDATE results,
+				# so currently there is no error handling here.
+			}
+		}
+		
+		return $this;
+	}
+	
+	
+	# Replace internal data with single associative array, and add new row to database
+	public function insert($datum, $primaryKey = false)
+	{
+		if($primaryKey === false) $primaryKey = $this->primaryKey;
+
+		$this->data = array($datum);
+		$this->save();
+		
+		$id = $this->db->insert_id();
+		$this->setValue($primaryKey, $id);
+		
+		return $this;
+	}
+
+
+	# Simple wrapper for CI's form validation, entirely optional
+	# Allows rules to be set in the model ($this->validation), rather than the controller
+	public function validate()
+	{
+		if(empty($this->validation)) return true;
+	
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules($this->validation);
+		
+		return $this->form_validation->run();
+	}
+	
+	
+	# No errors equals success, even if no internal data is present
+	public function success()
+	{
+		return !(count($this->errors) > 0);
+	}
+	
+	
+	# Push an error string onto the stack
+	public function error($errorString)
+	{
+		$this->errors[] = $errorString;
+		return $this;
+	}
+
+	
+	# Kill existing data with fire
+	public function clear()
+	{
+		$this->data = array();
+		$this->errors = array();
+		
+		return $this;
+	}
+	
+	# Erase internal errors
+	public function clearErrors()
+	{
+		$this->errors = array();
+		return $this;
+	}
+}
+
+# END class MY_Model
